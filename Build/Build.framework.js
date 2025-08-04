455c32c0cd6257896893561bf4fc9ae26e0c8d06
wf <weixin.huo@mvisioner.com>
2025-08-04 14:51:31 +0800

    rebuild project as webgl has broken; 1) need logo and splash 2) no compression , added decompression fallback, quality = mobile
    :

--- Build/Build.framework.js
+++ Build/Build.framework.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23c58f45e2f67d4a6fd7e08d659bd73761e61d05cca188efe6d5b399e10e3b68
-size 432932
+oid sha256:796dc212fdaf736f92a691e347821c9f6b184bd38f29ae2e6106065b4b6d1f3e
+size 451996
